643ce6d6bc23e285f5e99a54f4289a77925ca8b5
Samuel Oluwadare <oluwadaresamuel39@gmail.com>
2024-02-01 13:16:31 +0100

    fifth-commit

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
-export default function PageNotFound() {
+function PageNotFound() {
   return (
     <div>
       <h1>Page Not Found ):</h1>
     </div>
   );
 }
+
+export default PageNotFound;
